099b2bf53d0e50dd346b4d4e70c85764b724bc17
Kpearl <kpearl115@gmail.com>
2022-09-13 23:12:26 +0900

    add name

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login),
-    path('join/', views.join)
+    path('login/', views.login, name='login'),
+    path('join/', views.join, name='join')
 ]
